cd35a48c8c4a1a178bf400be8b7d8478dd8afa6b
carlkarama <carlkarama4@gmail.com>
2022-09-07 20:06:36 +1000

    feat: added base code

--- Core Animation/ViewController.swift
+++ Core Animation/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let coreAnimationLayer = CALayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coreAnimationLayer.backgroundColor = UIColor.systemRed.cgColor
+        coreAnimationLayer.frame = CGRect(x: 100, y: 100, width: 120, height: 120)
+        view.layer.addSublayer(coreAnimationLayer)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+3) {
+            self.animateMovements()
+        }
     }
-
-
+    
+    func animateMovements() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.fromValue = CGPoint(x: coreAnimationLayer.frame.origin.x + (coreAnimationLayer.frame.width/2),
+                                      y: coreAnimationLayer.frame.origin.y + (coreAnimationLayer.frame.height/2))
+        animation.toValue = CGPoint(x: 300, y: 400)
+        animation.duration = 3
+        animation.fillMode = .forwards // decides what to do when the animation is finished
+        animation.isRemovedOnCompletion = false //
+        animation.beginTime = CACurrentMediaTime()
+        coreAnimationLayer.add(animation, forKey: nil)
+        
+    }
 }
 
