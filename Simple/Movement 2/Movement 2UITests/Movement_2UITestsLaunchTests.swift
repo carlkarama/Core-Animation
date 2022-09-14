ae0835c454c146665fd15d19ed23a90f09f144c0
carlkarama <carlkarama4@gmail.com>
2022-09-14 18:11:15 +1000

    feat: movement 2 animation

---
+++ Simple/Movement 2/Movement 2UITests/Movement_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Movement_2UITestsLaunchTests.swift
+//  Movement 2UITests
+//
+//  Created by Carl Karama on 14/9/22.
+//
+
+import XCTest
+
+class Movement_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
